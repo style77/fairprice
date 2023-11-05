d948cabd01ea6a1ac98af4ecc753b863e167c8d8
style77 <stylek777@gmail.com>
2023-11-05 03:44:44 +0100

    feat: FairPrice main object

---
+++ fairprice/fairprice.py
@@ -0,0 +1,17 @@
+from typing import Type
+
+from fairprice.strategy.base import Strategy
+from fairprice.strategy.currency import Currency
+
+
+class FairPrice(object):
+    def __init__(self, strategy: Type[Strategy]):
+        self.strategy = strategy()
+
+    def balance(self, price: float, currency: str):
+        try:
+            currency_obj = Currency(currency.strip().lower())
+        except:
+            raise ValueError(f"No such currency as {currency}")
+
+        return self.strategy.calculate(price, currency_obj)
