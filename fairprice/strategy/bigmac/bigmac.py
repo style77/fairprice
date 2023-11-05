a969052de69724807ac8ea1353322be3581cb798
style77 <stylek777@gmail.com>
2023-11-05 03:44:56 +0100

    feat: bigmac strategy

---
+++ fairprice/strategy/bigmac/bigmac.py
@@ -0,0 +1,29 @@
+# BigMac Inde is a strategy invented by The Economist to compare prices of Big Macs in different countries.
+# It is based on the theory of purchasing-power parity (PPP) like all of the strategies included in this library,
+# the notion that in the long run exchange rates should move towards the rate that would equalise the prices of 
+# an identical basket of goods and services (in this case, a burger) in any two countries.
+# I just thought it would be fun to include it in this library.
+import pandas as pd
+
+from fairprice.strategy.base import DataType, Strategy
+from fairprice.strategy.currency import Currency
+
+
+# Dataset is updated till 2020, so data may be unaccurate.
+# Feel free to contribute and update big mac prices in your country
+class BigMac(Strategy):
+    DATA_TYPE = DataType.FILE
+    DATA = r"fairprice\strategy\bigmac\big_mac_index.csv"
+
+    def calculate(self, price: float, currency: Currency):
+        df = pd.read_csv(self.DATA)
+        df["date"] = pd.to_datetime(df["date"])
+        latest_data = df[df["date"] == df["date"].max()]
+        latest_data = latest_data[
+            latest_data["currency_code"] == currency.value.upper()
+        ]
+
+        big_mac_idx = (
+            latest_data["local_price"].values[0] / latest_data["dollar_price"].values[0]
+        )
+        return big_mac_idx * price
