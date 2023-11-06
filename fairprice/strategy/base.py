1ab72c2a102f225d12ca5d7c8e97ac96c447b513
style77 <stylek777@gmail.com>
2023-11-06 01:47:39 +0100

    chore: price object

--- fairprice/strategy/base.py
+++ fairprice/strategy/base.py
@@ -10,10 +10,19 @@ class DataType(Enum):
     FILE: str = "file"
 
 
+class Price(object):
+    def __init__(self, price: float, currency: Currency):
+        self.price = price
+        self.currency = currency
+
+    def __repr__(self) -> str:
+        return f"{self.price} {self.currency.value.upper()}"
+
+
 class Strategy(ABC):
     DATA_TYPE: typing.Optional[DataType] = None
     DATA: typing.Optional[str] = None
 
     @abstractmethod
-    def calculate(self, price: float, currency: Currency):
+    def calculate(self, price: float, currency: typing.Optional[Currency]) -> Price:
         raise NotImplementedError
